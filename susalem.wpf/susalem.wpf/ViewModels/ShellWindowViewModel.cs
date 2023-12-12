7eb8d71046cc82cfc76e0e3d7405abbaa82d8360
邵家驹 <987585547@qq.com>
2023-12-12 10:43:51 +0800

    wpf

---
+++ susalem.wpf/susalem.wpf/ViewModels/ShellWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace susalem.wpf.ViewModels
+{
+    public partial class ShellWindowViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        MenuItem selectedItem; 
+        [ObservableProperty]
+        ObservableCollection<MenuItem> menu = new()
+        {
+            new(){Name="Message",Icon="CommentDotsRegular"},
+            new(){Name="Community",Icon="CommentsRegular"},
+            new(){Name="Enterprise",Icon="BuildingRegular"}
+        };
+        public ShellWindowViewModel()
+        {
+            SelectedItem = Menu.FirstOrDefault();
+        }
+    }
+}
