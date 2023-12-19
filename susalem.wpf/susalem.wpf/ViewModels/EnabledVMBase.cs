93c7b53474a35e367bac2e6ad4fb2e9b468af537
邵家驹 <987585547@qq.com>
2023-12-19 23:23:25 +0800

    建立继承于observableobject并实现注册propertychangedmessage<bool>的enabledvmbase基类，需要实现shell中切换view和control模式改变自身部分按钮状态的viewmodel应继承此类

---
+++ susalem.wpf/susalem.wpf/ViewModels/EnabledVMBase.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace susalem.wpf.ViewModels
+{
+    public partial class EnabledVMBase : ObservableObject, IRecipient<PropertyChangedMessage<bool>>
+    {
+        [ObservableProperty]
+        protected bool isEnabled;
+        public EnabledVMBase()
+        {
+            RegisterIsEnabled();
+        }
+        public void Receive(PropertyChangedMessage<bool> message)
+        {
+            IsEnabled= message.NewValue;
+        }
+
+        protected void RegisterIsEnabled()
+        {
+            WeakReferenceMessenger.Default.Register<PropertyChangedMessage<bool>>(this);
+        }
+    }
+}
