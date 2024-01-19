886aab9c171d1cc1ac5e3c020334521e27532385
jyj <2534803898@qq.com>
2024-01-19 17:28:13 +0800

    commit sth

--- susalem.wpf/susalem.wpf/Services/IServices/IDataProvider.cs
+++ susalem.wpf/susalem.wpf/Services/IServices/IDataProvider.cs
@@ -20,13 +20,13 @@ namespace susalem.wpf.Services.IServices
 
             Task<HttpResponse<T>> PostData<T>(string url, object data);
 
-            ResultModel<string> CreateClientPost(string url, string parameters, int timeOutInMillisecond = 15000)
+            ResultModel<string> CreateClientPost(string url, string parameters, int timeOutInMillisecond = 15000);
 
-            ResultModel<T> CreateClientPost<T>(string url, string parameters, int timeOutInMillisecond = 15000)
+            ResultModel<T> CreateClientPost<T>(string url, string parameters, int timeOutInMillisecond = 15000);
 
-            ResultModel<string> CreateClientGet(string url, Dictionary<string, string> dic, int timeOutInMillisecond = 15000)
+            ResultModel<string> CreateClientGet(string url, Dictionary<string, string> dic, int timeOutInMillisecond = 15000);
 
-            ResultModel<T> CreateClientGet<T>(string url, Dictionary<string, string> dic, int timeOutInMillisecond = 15000)
+            ResultModel<T> CreateClientGet<T>(string url, Dictionary<string, string> dic, int timeOutInMillisecond = 15000);
 
         }
 
--- susalem.wpf/susalem.wpf/ViewModels/MessageViewModel.cs
+++ susalem.wpf/susalem.wpf/ViewModels/MessageViewModel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Windows;
 using System.Windows.Input;
 
 namespace susalem.wpf.ViewModels
