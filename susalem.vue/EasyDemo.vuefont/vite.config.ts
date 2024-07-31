41033a99cc87a83201e82d44f451a7df696cf1eb
Reyyyyyi <zhoufeng463708332@163.com>
2024-07-31 23:17:52 +0800

    配置unplugin-auto-import和unplugin-vue-components，初始化前端配置

--- susalem.vue/EasyDemo.vuefont/vite.config.ts
+++ susalem.vue/EasyDemo.vuefont/vite.config.ts
@@ -2,15 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
+  ]
 })
