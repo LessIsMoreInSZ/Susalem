3d7a25bd9c7b89f32d35496a422fafd37ec33344
JYJ <2534803898@qq.com>
2024-09-26 10:54:19 +0800

    s：  1.add a class 'studioObjectReader' to get a <objectname,model3d> keypair 2.add a class 'childSelectedableVisual3D' to save the keypair get from 1
    todo:
    1.when unselecting a model3d, it will lost it's former material

--- susalem.wpf/susalem.wpf/Visual3D/ChildSelectableVisual3D.cs
+++ susalem.wpf/susalem.wpf/Visual3D/ChildSelectableVisual3D.cs
@@ -1,6 +1,8 @@
 ﻿using HelixToolkit.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +11,28 @@ using System.Windows.Media.Media3D;
 
 namespace susalem.wpf.Visual3D
 {
-    public class ChildSelectableVisual3D:FileModelVisual3D
+    public class ChildSelectableVisual3D : FileModelVisual3D
     {
-        public Model3DGroup Group =>(Model3DGroup) this.Visual3DModel;
+        public Dictionary<string, Model3D> ChildrenDictionary { get; private set; }
+
+        protected override void SourceChanged()
+        {
+            var ext = Path.GetExtension(Source);
+            if (ext != null)
+            {
+                ext = ext.ToLower();
+            }
+            if (ext == ".3ds")
+            {
+                var r = new StudioMeshDictionaryReader(Dispatcher) { DefaultMaterial = DefaultMaterial, Freeze = false };
+                (this.Visual3DModel, ChildrenDictionary) = r.Read(Source);
+            }
+            else
+            {
+                var importer = new ModelImporter { DefaultMaterial = this.DefaultMaterial };
+                this.Visual3DModel = this.Source != null ? importer.Load(this.Source) : null;
+            }
+            this.OnModelLoaded();
+        }
     }
 }
