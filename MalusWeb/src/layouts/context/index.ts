65f1eefadc14b04289a635729279b4c99715beea
HoboCY <1830231903@qq.com>
2024-10-22 15:46:04 +0800

    chore: 删除多余前端项目

--- MalusWeb/src/layouts/context/index.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from '@sa/hooks';
-import { useMixMenu } from '../hooks';
-
-export const { setupStore: setupMixMenuContext, useStore: useMixMenuContext } = useContext('mix-menu', useMixMenu);
--- MalusWeb/src/utils/storage.ts
+++
@@ -1,7 +0,0 @@
-import { createLocalforage, createStorage } from '@sa/utils';
-
-export const localStg = createStorage<StorageType.Local>('local');
-
-export const sessionStg = createStorage<StorageType.Session>('session');
-
-export const localforage = createLocalforage<StorageType.Local>('local');
